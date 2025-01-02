65935cd1ff9631201c899b89d4234a7dba49590e
Suhyun Jang <honan24@naver.com>
2025-01-03 06:56:06 +0900

    feat: Domain 패키지 구조 구현
    - coupon: 쿠폰 관련 서비스
    - order: 주문 관련 서비스
    - payment: 결제 관련 서비스
    - product: 상품 관련 서비스
    - user: 유저 관련 서비스

---
+++ src/main/java/kr/hhplus/be/server/domain/payment/PaymentService.java
@@ -0,0 +1,9 @@
+package kr.hhplus.be.server.domain.payment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+}
