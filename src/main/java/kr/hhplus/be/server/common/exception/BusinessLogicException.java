aa9a67f45944be29a96c2f65b2479faae8f9f36c
Suhyun Jang <honan24@naver.com>
2025-01-05 22:46:22 +0900

    feat: 비즈니스 로직의 예외처리를 위한 exception 파일 생성

--- src/main/java/kr/hhplus/be/server/common/exception/BusinessLogicException.java
+++ src/main/java/kr/hhplus/be/server/common/exception/BusinessLogicException.java
@@ -1,4 +1,10 @@
 package kr.hhplus.be.server.common.exception;
 
-public class BusnessLogicException {
+public class BusinessLogicException extends RuntimeException {
+    private final ExceptionCode exceptionCode;
+
+    public BusinessLogicException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
 }
--- src/main/java/kr/hhplus/be/server/common/exception/ExceptionCode.java
+++ src/main/java/kr/hhplus/be/server/common/exception/ExceptionCode.java
@@ -1,2 +1,13 @@
-package kr.hhplus.be.server.common.exception;public class exceptionCode {
+package kr.hhplus.be.server.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ExceptionCode {
+    NOT_FOUND(400, "not found");
+
+    private final int code;
+    private final String message;
 }
