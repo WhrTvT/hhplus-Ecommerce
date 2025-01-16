ab2266d7257c82a869ccca0bbe21f20f935747be
Suhyun Jang <honan24@naver.com>
2025-01-17 02:54:02 +0900

    chore: 데이터플랫폼 Mock API 수정

--- src/main/java/kr/hhplus/be/server/interfaces/mock/DataPlatformService.java
+++ src/main/java/kr/hhplus/be/server/interfaces/mock/DataPlatformService.java
@@ -1,23 +1,12 @@
 package kr.hhplus.be.server.interfaces.mock;
 
 import kr.hhplus.be.server.application.out.PaymentInfo;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class DataPlatformService {
-    private final RestTemplate restTemplate;
-
-    public DataPlatformService(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
-    }
-
     public String sendPaymentToMockPlatform(PaymentInfo paymentInfo) {
-        String mockApiUrl = "http://localhost:8080/mock/dataPlatform";
-        ResponseEntity<String> response = restTemplate.postForEntity(mockApiUrl, paymentInfo, String.class);
 
-        return response.getBody();
+        return "데이터플랫폼 전송 성공";
     }
 }
